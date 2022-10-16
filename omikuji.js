9b8c6348355150a490a75a957c00424e98fe7a39
skmthrhk9280 <skmthrhk9280@gmail.com>
2022-10-16 15:14:11 +0900

    change

--- omikuji.js
+++ omikuji.js
@@ -3,12 +3,13 @@ function question(){
     let celebrate=document.getElementById("celebrate");
     let omikuji = ["大吉","吉","中吉","小吉","末吉","凶","大凶"];
     let r = Math.floor( Math.random() * omikuji.length) ;
-    let celesound= new Audio("celebrate.mp3")
-    console.log(r);
+    let celesound= new Audio("celebrate.mp3");
+
     result.innerHTML=omikuji[r];
-    celebrate.innerHTML="";
+
     if(r==0){
         celesound.play();
         celebrate.innerHTML="大吉です！！！おめでとうございます！！！！";
     }
+
 }
