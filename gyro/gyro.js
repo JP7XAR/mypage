50793acbfaaa953887404add877314be48b68f84
skmthrhk9280 <skmthrhk9280@gmail.com>
2023-01-01 20:58:35 +0900

    加速度センサー用のページを追加

---
+++ gyro/gyro.js
@@ -0,0 +1,21 @@
+function getgyro(){
+    let gyro=document.getElementById("gyro");
+    
+    window.addEventListener('devicemotion', function(e) {
+        var str = '',
+            x   = e.acceleration.x,
+            y   = e.acceleration.y,
+            z   = e.acceleration.z;
+    
+        str  = 'x = ' + x + '\n';
+        str += 'y = ' + y + '\n';
+        str += 'z = ' + z + '\n';
+    
+        //alert(str);
+    }, false);
+    gyro.innerHTML=str
+}
+
+window.onload=function(){
+    getgyro()
+}
